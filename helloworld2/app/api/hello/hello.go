46f74701bb377bd613d4ed28d0331869b8c98d16
小周 <602068971@qq.com>
2019-10-30 00:21:18 +0800

    test: 微服务 单仓库实践

--- helloworld2/app/api/hello/hello.go
+++ helloworld2/app/api/hello/hello.go
@@ -2,12 +2,15 @@ package hello
 
 import (
 	"github.com/gogf/gf/net/ghttp"
+	"github.com/gogf/gf/os/glog"
 	"natsmicro/helloworld/rpc/nrpc_client"
 	"natsmicro/helloworld/rpc/proto/helloworld"
 )
 
 // Hello World
 func Handler(r *ghttp.Request) {
-	nrpc_client.HelloWordNrpcCli.SayHello(helloworld.HelloRequest{Name: "world"})
-	r.Response.Writeln("Hello World!")
+	res, _ := nrpc_client.HelloWordNrpcCli.SayHello(helloworld.HelloRequest{Name: "world"})
+	glog.Info(res.Message)
+
+	r.Response.Writeln("Hello World!" + res.Message)
 }
